0340685737f65db4504cbc6fd5ad40ef86236469
Steven Deng <deng.steven@gmail.com>
2013-08-12 04:11:33 -0400

    added link to pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,7 +3,7 @@ class Pin < ActiveRecord::Base
 #rails validations
 validates :description, presence: true
 validates :user_id, presence: true
-has_attached_file :image, styles: { medium: "320x240"}
+has_attached_file :image, styles: { medium: "320x240", thumb: "100x100"}
 validates_attachment :image, presence: true,
 							 content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']},
 							 size: { less_than: 5.megabytes }
