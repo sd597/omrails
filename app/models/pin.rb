4a138e3bb39cb779fbc9afd3dee4e8d363319987
Steven Deng <deng.steven@gmail.com>
2013-08-11 22:18:20 -0400

    add pins assocation

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,8 @@
 class Pin < ActiveRecord::Base
 #rails validations
 validates :description, presence: true
+
+#realtionships --> rails associations
+belongs_to :user
+validates :user_id, presence:true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, #:recoverable,
          :rememberable, :trackable, :validatable
+
+	#realtionships --> rails associations
+ 	has_many :pins
+
 end
